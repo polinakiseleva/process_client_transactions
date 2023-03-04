beafc7e88392146a6d90f4c893ad2a40c02c0dc5
Polina <polinakiseleva4@gmail.com>
2023-03-04 11:00:51 +0200

    Add func to utils.py

---
+++ utils/utils.py
@@ -0,0 +1,11 @@
+import requests
+
+URL = 'https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp=1678006423443&signature=LrRvgBmzJUjZRcDynWElQuoPThOTcQ7TWHsT_dTyv0I&downloadName=operations.json'
+
+
+def func(par):
+    responce = requests.get(par)
+    result = responce.json()
+    return result
+
+
